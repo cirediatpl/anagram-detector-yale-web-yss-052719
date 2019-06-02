d8f8354d09c2e84c5af9035a1b5e88488db113fc
Peter <cirediatpl@gmail.com>
2019-06-02 15:34:34 -0400

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,16 @@
-# Your code goes here!
+class Anagram
+
+    attr_accessor :word
+
+    def initialize(word)
+        @word = word
+    end
+
+    def match(array)
+        array.select do |some_word|
+            some_word.split("").sort == self.word.split("").sort
+        end
+        ##returns element of array that is an anagram of @string
+    end
+
+end
